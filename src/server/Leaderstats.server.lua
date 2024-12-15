b7ce62abeccee1f146156beb7bcced1c4754a1a3
manny232z <117049752+manny232z@users.noreply.github.com>
2024-12-14 20:12:46 -0600

    more stuff

---
+++ src/server/Leaderstats.server.lua
@@ -0,0 +1,11 @@
+local Players = game:GetService("Players")
+
+Players.PlayerAdded:Connect(function(player)
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = player
+
+    local kills = Instance.new("IntValue")
+    kills.Name = "kills"
+    kills.Parent = leaderstats
+end)
